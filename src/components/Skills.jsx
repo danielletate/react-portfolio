8b34f9e2ed4c58704391d043f35616f0a42c9715
Danielle Tate <danielletate@Danielles-MacBook-Air.local>
2022-11-04 12:14:54 -0600

    work component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,16 +7,17 @@ import Vue from '../assets/vue.png';
 import Git from '../assets/git.png';
 import Tailwind from '../assets/tailwind.png';
 import Bootstrap from '../assets/bootstrap.png';
+import Squarespace from '../assets/squarespace.png'
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-gray-800 text-gray-300'>
+    <div name='skills' className='w-full bg-gray-800 text-gray-300'>
 
     {/* Container */}
-    <div className='w-full h-full max-w-[1000px] mx-auto px-5 flex flex-col justify-center'>
+    <div className='w-full max-w-[1000px] mx-auto px-5 flex flex-col justify-center'>
         <div className='py-2'>
-            <p className='text-4xl font-bold inline border-b-4 border-violet-500 '>Skills</p>
-            <p className='py-5 max-w-sm sm:max-w-lg'>The skills, tools & technologies I use to bring your products to life.</p>
+            <p className='text-4xl font-bold inline border-b-4 border-violet-500'>Skills</p>
+            <p className='py-6 max-w-sm sm:max-w-lg'>The skills, tools & technologies I use to bring your products to life.</p>
         </div>
 
         {/* Icons and text */}
@@ -52,6 +53,10 @@ const Skills = () => {
             <div className='uppercase shadow-md shadow-[#040720] hover:scale-110 duration-500 cursor-pointer'>
                 <img className='w-20 mx-auto' src={Bootstrap} alt="Bootstrap Icon" />
                 <p className='my-4 uppercase text-sm'>Bootstrap</p>
+            </div>
+            <div className='uppercase shadow-md shadow-[#040720] hover:scale-110 duration-500 cursor-pointer'>
+                <img className='w-20 mx-auto' src={Squarespace} alt="Squarespace Icon" />
+                <p className='my-4 uppercase text-sm'>Squarespace</p>
             </div>
         </div>
     </div>
---
+++ src/components/Work.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+import WorkImg1 from '../assets/projects/work1-placeholder.jpg'
+import WorkImg2 from '../assets/projects/work2-placeholder.jpg'
+
+
+const Work = () => {
+  return (
+    <div name="work" className="w-full bg-gray-800 text-gray-300 md:h-screen">
+
+        {/* Container */}
+        <div className='w-full h-full max-w-[1000px] mx-auto px-5 flex flex-col justify-center'>
+            <div className='py-2 pb-6'>
+                <p className='text-4xl font-bold inline border-b-4 border-violet-500'>Work</p>
+                <p className='py-6 md:max-w-full max-w-sm'>
+                  Below are a few past projects that I've worked on. Want to see more?
+                  <a className='text-violet-500 ml-1 font-medium' href="mailto:danielle.tate825@gmail.com">
+                    Email me
+                  </a>
+                </p>
+            </div>
+
+            {/* Grid Container */}
+            <div 
+              className='grid gap-4 grid-cols-1 md:grid-cols-3 px-2'>
+
+                {/*Grid Item */}
+                <div 
+                  style={{backgroundImage: `url(${WorkImg1})`}} 
+                  className='shadow-lg shadow-[#040720] container rounded-md flex justify-center items-center mx-auto content-div group'>
+                    
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl text-white tracking-wider font-bold'>
+                            React JS Application
+                        </span>
+
+                        <div className='pt-10 text-center'>
+                            <a href="#">
+                               <button className='text-lg text-center bg-white text-gray-700 font-bold rounded-lg px-4 py-3 m-2'>Demo</button>
+                            </a>
+                            <a href="#">
+                                <button className='text-lg text-center bg-green-500 text-white font-bold rounded-lg px-4 py-3 m-2'>Code</button>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+
+                {/* Grid Item */}
+                <div 
+                  style={{backgroundImage: `url(${WorkImg2})`}} 
+                  className='shadow-lg shadow-[#040720] container rounded-md flex justify-center items-center mx-auto content-div group'>
+                    
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl text-white tracking-wider font-bold'>
+                            React JS Application
+                        </span>
+
+                        <div className='pt-10 text-center'>
+                            <a href="#">
+                               <button className='text-lg text-center bg-white text-gray-700 font-bold rounded-lg px-4 py-3 m-2'>Demo</button>
+                            </a>
+                            <a href="#">
+                                <button className='text-lg text-center bg-green-500 text-white font-bold rounded-lg px-4 py-3 m-2'>Code</button>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div 
+                  style={{backgroundImage: `url(${WorkImg1})`}} 
+                  className='shadow-lg shadow-[#040720] container rounded-md flex justify-center items-center mx-auto content-div group'>
+                    
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl text-white tracking-wider font-bold'>
+                            React JS Application
+                        </span>
+
+                        <div className='pt-10 text-center'>
+                            <a href="#">
+                               <button className='text-lg text-center bg-white text-gray-700 font-bold rounded-lg px-4 py-3 m-2'>Demo</button>
+                            </a>
+                            <a href="#">
+                                <button className='text-lg text-center bg-green-500 text-white font-bold rounded-lg px-4 py-3 m-2'>Code</button>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+
+                {/* Grid Item */}
+                <div 
+                  style={{backgroundImage: `url(${WorkImg2})`}} 
+                  className='shadow-lg shadow-[#040720] container rounded-md flex justify-center items-center mx-auto content-div group'>
+                    
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl text-white tracking-wider font-bold'>
+                            React JS Application
+                        </span>
+
+                        <div className='pt-10 text-center'>
+                            <a href="#">
+                               <button className='text-lg text-center bg-white text-gray-700 font-bold rounded-lg px-4 py-3 m-2'>Demo</button>
+                            </a>
+                            <a href="#">
+                                <button className='text-lg text-center bg-green-500 text-white font-bold rounded-lg px-4 py-3 m-2'>Code</button>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Work
